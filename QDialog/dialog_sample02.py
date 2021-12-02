8833be6187cbf88bd02830f429635cb91f06783f
yamagishi <tayama27@gmail.com>
2021-12-02 13:35:08 +0900

    Adding exmaples

---
+++ QDialog/dialog_sample02.py
@@ -0,0 +1,9 @@
+import sys
+
+from PySide2 import QtWidgets
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+    result = dialog.exec_()
+    print(result)
